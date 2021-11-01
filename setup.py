5a81893109f8c89549df7c4806aec9a70fbcb3a6
G_will <g_will@ieqi.com>
2021-11-01 09:01:47 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qtl-instrument-book',
-    version='20211015',
+    version='20211101',
     description='Quantalon Instrument Book',
     packages=find_packages(),
     include_package_data=True,
