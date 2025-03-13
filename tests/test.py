1bac399c1deb817fb026698a3b4bd4e757d3310e
G_will <g_will@ieqi.com>
2025-03-13 14:57:58 +0800

    update

--- tests/test.py
+++ tests/test.py
@@ -3,25 +3,25 @@ from qtl_instrument_book import OptionsInstrumentBook
 
 
 def test():
-    symbol = 'INE.sc2203'
+    symbol = 'INE.sc2509'
     futures_instrument_book = FuturesInstrumentBook()
 
     i = futures_instrument_book.get_instrument(symbol)
     print(i)
 
-    i = futures_instrument_book.get_instrument_by_id('sc2203')
+    i = futures_instrument_book.get_instrument_by_id('sc2509')
     print(i)
 
     r = futures_instrument_book.is_trading_time(symbol)
     print(r)
 
-    symbol = 'CFFEX.IO2202-C-5000'
+    symbol = 'CFFEX.IO2504-C-3900'
     options_instrument_book = OptionsInstrumentBook()
 
     i = options_instrument_book.get_instrument(symbol)
     print(i)
 
-    i = options_instrument_book.get_instrument_by_id('IO2202-C-5000')
+    i = options_instrument_book.get_instrument_by_id('IO2504-C-3900')
     print(i)
 
     r = options_instrument_book.is_trading_time(symbol)
